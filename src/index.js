888601170e9a22d3b18c1d45f8e8cc15862bf049
Bowen Wang <iwangbowen@gmail.com>
2018-08-22 15:16:52 +0800

    Add file upload endpoint

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import initializeDb from './db';
 import middleware from './middleware';
 import api from './api';
 import config from './config.json';
+const fileUpload = require('express-fileupload');
 
 let app = express();
 app.server = http.createServer(app);
@@ -20,11 +21,26 @@ app.use(cors({
 }));
 
 app.use(bodyParser.json({
-	limit : config.bodyLimit
+	limit: config.bodyLimit
 }));
 
+app.use(fileUpload());
+
+app.post('/api/upload', function (req, res) {
+	if (!req.files)
+		return res.status(400).send('No files were uploaded.');
+	// The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+	Object.values(req.files).forEach(file => {
+		file.mv(`./upload/${file.name}`, err => {
+			if (err)
+				return res.status(500).send(err);
+			res.send('File uploaded!');
+		});
+	});
+});
+
 // connect to db
-initializeDb( db => {
+initializeDb(db => {
 
 	// internal middleware
 	app.use(middleware({ config, db }));
