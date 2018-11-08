b6a172b281b9752c0ad8ed1ad610059823ce6d89
Bowen Wang <iwangbowen@gmail.com>
2018-11-08 12:35:41 +0800

    Add single data api

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,7 @@ import companies from './companies';
 import countries from './countries';
 import data from './data';
 import multi_data from './multi-data';
+import single_data from './single-data.js';
 import upload from './upload';
 
 export default ({ config, db }) => {
@@ -16,6 +17,7 @@ export default ({ config, db }) => {
 	api.use('/countries', countries({ config, db }));
 	api.use('/data', data({config, db}));
 	api.use('/multidata', multi_data({config, db}));
+	api.use('/singledata', single_data({config, db}));
 	api.use('/upload', upload({config, db}));
 
 	// perhaps expose some API metadata at the root
